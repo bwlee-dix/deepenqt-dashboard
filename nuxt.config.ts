bcb8cdd05658c6a12cb3fb5196b9ea0c5e5e4321
bwlee-dix <137477179+bwlee-dix@users.noreply.github.com>
2025-09-23 16:50:30 +0900

    feat: Initial environment settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,47 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/image']
-})
+  srcDir: "src/",
+  ssr: false,
+  css: ["~/asset/css/tailwind.css"],
+  components: [
+    {
+      path: "~/component",
+    },
+  ],
+  app: {
+    head: {
+      title: "DeepenQT Dashboard",
+      meta: [
+        { name: "description", content: "Deepen QT Dashboard" },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1, maximum-scale=1",
+        },
+      ],
+    },
+  },
+  dir: {
+    pages: "page",
+    layouts: "layout",
+  },
+  imports: {
+    dirs: ["composable, composable/**"],
+  },
+  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/image", "shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./lib/ui"
+     */
+    componentDir: "./lib/ui",
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ src/page/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import { Button } from "@/lib/ui/button";
+</script>
+<template>
+  <h1>DeepenQT Dashboard</h1>
+  <Button>Click me</Button>
+</template>
+
+<style scoped></style>
